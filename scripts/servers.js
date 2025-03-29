44adaee0b3ea2073b6fd11baa005973de1cdb35e
Jaimy Tuin <69647075+JaimyTuin223@users.noreply.github.com>
2025-03-29 15:22:35 +0000

    mobile support

--- scripts/servers.js
+++ scripts/servers.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const serverList = document.getElementById("serverList");
     const socials = document.getElementById("socials");
 
+    // Mobile
+    const mobileServerList = document.getElementById("mobileServerList");
+    const mobileSocials = document.getElementById("mobileSocials");
+
+    // Server data
     const serverDetails = {
         "GingerProductions.jpeg": {
             title: "Ginger Productions",
@@ -16,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
+    // Show server information
     serverList.addEventListener("click", (event) => {
         if (event.target.tagName === "IMG") {
             const imageName = event.target.src.split("/").pop();
@@ -30,11 +36,33 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    mobileServerList.addEventListener("click", (event) => {
+        if (event.target.tagName === "IMG") {
+            const imageName = event.target.src.split("/").pop();
+            const details = serverDetails[imageName];
+
+            if (details) {
+                let mobileInfoBox = document.getElementById("mobileServerInfo");
+                if (!mobileInfoBox) return console.log("No mobileInfoBox found")
+
+                mobileInfoBox.innerHTML = `<h3>${details.title}</h3><p>${details.description}</p><a href="${details.serverURL}">Join Now</a>`; // <span class="external material-symbols-outlined">open_in_new</span>
+            }
+        }
+    });
+
+    // Hide server information
     document.addEventListener("click", (event) => {
         if (!socials.contains(event.target)) {
             const infoBox = document.getElementById("serverInfo");
             if (infoBox) {
                 infoBox.innerHTML = "";
+            }
+        }
+
+        if (!mobileSocials.contains(event.target)) {
+            const mobileInfoBox = document.getElementById("mobileServerInfo");
+            if (mobileInfoBox) {
+                mobileInfoBox.innerHTML = "";
             }
         }
     });
